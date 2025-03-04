c905642004020d2eaa7fe586f5a83e8d87bb6caa
Funiek01 <piotrr473@gmail.com>
2025-03-04 10:38:53 +0100

    Added admin component and login component to get more it more clear

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'admin', component: AdminDashboardComponent},
+    { path: '**', redirectTo: '/home' },
+];
