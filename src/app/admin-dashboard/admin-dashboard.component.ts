4e873b9bc57a6327fbf5e9dd2430c71d27fbe969
Funiek01 <piotrr473@gmail.com>
2025-03-04 11:10:20 +0100

    Admin dashboard fixed view

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
 export class AdminDashboardComponent {
-
+  employees = [
+    { id: 1, firstName: "Vincent", lastName: "Vega", reportsTo: "Bill Kamis", email: "vincent.vega@gmail.com", startDate: "2001-05-27" },
+    { id: 2, firstName: "Jules", lastName: "Winnfield", reportsTo: "Marsellus Wallace", email: "jules.winnfield@gmail.com", startDate: "1999-11-10" },
+    { id: 3, firstName: "Winston", lastName: "Wolfe", reportsTo: "Marsellus Wallace", email: "winston.wolfe@gmail.com", startDate: "1995-06-05" },
+    { id: 4, firstName: "Marsellus", lastName: "Wallace", reportsTo: "Self", email: "marsellus.wallace@gmail.com", startDate: "1990-02-14" },
+    { id: 5, firstName: "Butch", lastName: "Coolidge", reportsTo: "No One", email: "butch.coolidge@gmail.com", startDate: "1993-08-21" }
+  ];
 }
