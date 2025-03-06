f225210d8a57ea210fd49b0f510c194263148a06
Neetye-D <neetyedoorgesh@gmail.com>
2025-03-06 08:55:54 +0400

    added employee dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'admin', component: AdminDashboardComponent},
     {path: 'add-user', component: AddUserComponent},
+
+    {path: 'employee', component: EmployeeDashboardComponent},
+
     { path: '**', redirectTo: '/home' },
 ];
