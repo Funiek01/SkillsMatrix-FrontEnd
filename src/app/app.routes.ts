73f26ff665a88744537216922d85c295962099be
Ayesha <ayesumser17@gmail.com>
2025-03-06 06:13:01 +0400

    added manager feedback page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { ManagerFeedbackComponent } from './manager-feedback/manager-feedback.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'admin', component: AdminDashboardComponent},
     {path: 'add-user', component: AddUserComponent},
+    {path: 'manager-feedback', component: ManagerFeedbackComponent},
     { path: '**', redirectTo: '/home' },
 ];
